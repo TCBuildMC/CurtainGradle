e1a19441e1b667aef049d2812ff2f776c0a4eb75
Mr_limr267 <113236317+erha134@users.noreply.github.com>
2024-06-26 21:34:52 +0800

    Gradle DSL 支持

--- src/main/groovy/xyz/tcbuildmc/minecraft/curtaingradle/CurtainGradleExtension.groovy
+++ src/main/groovy/xyz/tcbuildmc/minecraft/curtaingradle/CurtainGradleExtension.groovy
@@ -1,6 +1,8 @@
 package xyz.tcbuildmc.minecraft.curtaingradle
 
+import org.gradle.api.Action
 import org.gradle.api.Project
+import xyz.tcbuildmc.minecraft.curtaingradle.extension.Dependencies
 import xyz.tcbuildmc.minecraft.curtaingradle.extension.Metadata
 import xyz.tcbuildmc.minecraft.curtaingradle.extension.Repositories
 
@@ -16,4 +18,18 @@ class CurtainGradleExtension {
     Repositories repositories = new Repositories(project.repositories)
 
     Metadata metadata = new Metadata()
+
+    Dependencies dependencies = new Dependencies()
+
+    def repositories(Action<? super Repositories> action) {
+        action.execute repositories
+    }
+
+    def metadata(Action<? super Metadata> action) {
+        action.execute metadata
+    }
+
+    def dependencies(Action<? super Dependencies> action) {
+        action.execute dependencies
+    }
 }
---
+++ src/main/groovy/xyz/tcbuildmc/minecraft/curtaingradle/extension/Dependencies.groovy
@@ -0,0 +1,23 @@
+package xyz.tcbuildmc.minecraft.curtaingradle.extension
+
+class Dependencies {
+    String bukkitApi(String version) {
+        return "org.bukkit:bukkit:${version}"
+    }
+
+    String spigotApi(String version) {
+        return "org.spigotmc:spigot-api:${version}"
+    }
+
+    String paperApi(String version) {
+        return "io.papermc:paper-api:${version}"
+    }
+
+    String purpurApi(String version) {
+        return "org.purpurmc.purpur:purpur-api:${version}"
+    }
+
+    String leavesApi(String version) {
+        return "org.leavesmc.leaves:leaves-api:${version}"
+    }
+}
