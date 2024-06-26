04435a27eb2d623de666184713e6667218bd215a
Mr_limr267 <113236317+erha134@users.noreply.github.com>
2024-06-26 19:06:44 +0800

    UrlBuilderTest 迁移

---
+++ src/test/groovy/xyz/tcbuildmc/minecraft/curtainlib/gradle/util/UrlBuilderTest.groovy
@@ -0,0 +1,25 @@
+package xyz.tcbuildmc.minecraft.curtainlib.gradle.util
+
+import org.junit.jupiter.api.Test
+
+class UrlBuilderTest {
+    @Test
+    void paperTest() {
+        println UrlBuilder.paper("1.21")
+    }
+
+    @Test
+    void purpurTest() {
+        println UrlBuilder.purpur("1.20.6")
+    }
+
+    @Test
+    void leavesTest() {
+        println UrlBuilder.leaves("1.20.6")
+    }
+
+    @Test
+    void mohistTest() {
+        println UrlBuilder.mohist("1.20.1")
+    }
+}
